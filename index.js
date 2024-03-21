409ffe1298555ee2b5ad7e11f176df62c97c48ad
malix254 <griffinskirimimbae@mail.com>
2024-03-22 02:38:56 +0300

    updated

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+
+// Write your code here//
+
+let num1= 31;
+let num2= 2;
+
+let multiply= num1 * num2 
+
+let random = Math.ceil(Math.random() * 10)
+
+// Define the variables num3 and num4//
+let num3 = 20;
+let num4 = 16;
+// Create the variable mod and set it to the equation//
+let mod = num3 % num4 ;
+//define max and do whatever is needed//
+
+let numbers = [2, 10, 17, 20];
+let max = Math.max(...numbers);
